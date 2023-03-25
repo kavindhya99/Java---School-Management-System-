f66852e67a313fa6bf34f3313433eaab7c495c3b
kavindhya99 <123450041+kavindhya99@users.noreply.github.com>
2023-03-25 21:35:45 +0530

    first commit

---
+++ src/databaseConnection.java
@@ -0,0 +1,28 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class databaseConnection {
+    static Connection connection(){
+    try{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/schoolmanagement?verifyServerCertificate=false&useSSL=true","root","");
+        return con;
+    
+    }catch(Exception e){
+     System.out.println(e);
+     return null;
+    }
+    }
+    
+}
